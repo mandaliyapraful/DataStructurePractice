9959ae70206dd1099b3afe2388f47a2bc0f34237
mandaliyapraful <mandaliyapraful@gmail.com>
2018-09-13 15:39:58 +0530

    Binary tree Impl with pre,pos and inOrder traversal along with levelOrder traversal

---
+++ src/com/datastructure/tree/BinaryTree.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package com.datastructure.tree;
+
+/**
+ * @author mandaliya
+ *
+ */
+public class BinaryTree {
+	
+	//Root Node
+	Node root;
+	
+	public BinaryTree(int key) {
+		root = new Node(key);
+	}
+	
+	public BinaryTree() {
+		root = null;
+	}
+	
+	public void display(){
+		System.out.println("Tree");
+		while(root != null){
+			System.out.println(" "+root.key+" ");
+			System.out.println(" /   \\");
+			System.out.println(root.left.key+"   "+root.right.key);
+			root = root.left;
+		}
+	}
+	public void InOrder(){
+		printInOrder(root);
+	}
+	private void printInOrder(Node node) {
+		if(node == null){
+			return;
+		}
+		
+		//Left
+		printInOrder(node.left);
+		
+		//print
+		System.out.print(node.key+" ");
+		
+		//right
+		printInOrder(node.right);
+	}
+
+	
+	public void printpreOrder(Node node){
+		if(node == null){
+			return;
+		}
+		//print root
+		System.out.print(node.key+" ");
+		//visit left
+		printpreOrder(node.left);
+		//visit right
+		printpreOrder(node.right);
+	}
+	
+	public void printPostOrder(Node node){
+		if(node == null){
+			return;
+		}
+		//visit left
+		printPostOrder(node.left);
+		//vist right
+		printPostOrder(node.right);
+		//print root
+		System.out.print(node.key+" ");
+		
+	}
+	
+	public void levelOrder(){
+		int h = height(root);
+		int i;
+		System.out.println(h);
+		for(i = 1;i <= h;i++){
+			printLeveLOrder(root,i);
+		}
+	}
+	private void printLeveLOrder(Node node, int level) {
+		if(node == null){
+			return;
+		}
+		if(level == 1){
+			System.out.print(node.key+" ");
+		}else if(level > 1){
+			//print left
+			printLeveLOrder(node.left, level-1);
+			//print right
+			printLeveLOrder(node.right, level-1);
+		}
+	}
+
+	private int height(Node node) { 
+		if(node == null){
+			return 0;
+		}else {
+			int lheight = height(node.left);
+			int rheight = height(node.right);
+			if(lheight >= rheight){
+				return (lheight+1);
+			}else{
+				return (rheight+1);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		BinaryTree binaryTree = new BinaryTree();
+		
+		binaryTree.root = new Node(1);
+		
+		binaryTree.root.left = new Node(2);
+		
+		binaryTree.root.right = new Node(3);
+		
+		binaryTree.root.right.left = new Node(6);
+
+		binaryTree.root.right.right = new Node(7);
+		
+		binaryTree.root.left.left = new Node(4);
+		
+		binaryTree.root.left.right = new Node(5);
+		
+		System.out.println("InOrder");
+		binaryTree.printInOrder(binaryTree.root);
+		System.out.println();
+		System.out.println("PreOder");
+		binaryTree.printpreOrder(binaryTree.root);
+		System.out.println();
+		System.out.println("PostOrder");
+		binaryTree.printPostOrder(binaryTree.root);
+		System.out.println("LevelOrder");
+		System.out.println();
+		binaryTree.levelOrder();
+		
+	}
+
+}
---
+++ src/com/datastructure/tree/Node.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.datastructure.tree;
+
+/**
+ * @author mandaliya
+ *
+ */
+public class Node {
+	int key;
+	Node left,right;
+	public Node(int key) {
+		this.key = key;
+		left = right = null;
+	}
+
+}
