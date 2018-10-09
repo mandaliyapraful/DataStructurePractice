5a8e43e4aa9ee66dfc62d0319ad0bd67c70edc6b
mandaliyapraful <mandaliyapraful@gmail.com>
2018-10-09 08:53:40 +0530

    Removing Duplicate from unsorted Linkedlist

--- src/com/datastructure/LinkedList/RemoveDuplicateFromLList.java
+++ src/com/datastructure/LinkedList/RemoveDuplicateFromLList.java
@@ -8,21 +8,36 @@ package com.datastructure.LinkedList;
  *
  */
 public class RemoveDuplicateFromLList {
-	
-	public void removeDup(Node head){
-		Node next = head.next;
-			
+
+	public void removeDup(Node head) {
+		Node next = null;
+		while (head != null && head.next != null) {
+			next = head;
+			while (next.next != null) {
+				if (head.data == next.next.data) {
+					next.next = next.next.next;
+				} else {
+					next = next.next;
+				}
+			}
+			head = head.next;
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		RemoveDuplicateFromLList duplicateFromLList = new RemoveDuplicateFromLList();
 		LinkedList llist = new LinkedList();
-		llist.push(1);
-		llist.push(2);
-		llist.push(3);
 		llist.push(6);
+		llist.push(1);
+		llist.push(6);
+		llist.push(2);
+		llist.push(6);
+		llist.push(6);
+		llist.push(3);
+		// llist.display();
+		// System.out.println(llist.head.data);
+		duplicateFromLList.removeDup(llist.head);
 		llist.display();
-		System.out.println(llist.head.data);
 	}
 
 }
